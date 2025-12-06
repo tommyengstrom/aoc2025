059fb3a56b4ed434cbc692e231f844d20279fd95
Tommy Engström <tommy@succinct.se>
2025-12-06 07:19:37 +0100

    Day 3, part 2

--- src/bin/day03.rs
+++ src/bin/day03.rs
@@ -5,12 +5,15 @@ fn parse_input(s: &str) -> Vec<Option<Bank>> {
 }
 
 fn parse_bank(s: &str) -> Option<Bank> {
-    let a: Option<Vec<u32>> = s.chars().map(|c| c.to_digit(10)).collect();
+    let a: Option<Vec<u64>> = s
+        .chars()
+        .map(|c| c.to_digit(10).map(|d| d as u64))
+        .collect();
     a.map(|d| Bank { batteries: d })
 }
 
 struct Bank {
-    batteries: Vec<u32>,
+    batteries: Vec<u64>,
 }
 
 fn main() {
@@ -24,36 +27,57 @@ fn main() {
             None => panic!("Row {i} of input is fucked"),
         })
         .collect();
-    let total: u32 = banks.iter().map(max_jolts).sum();
+    let total2: u64 = banks.iter().map(|x| max_jolts(2, x)).sum();
+    let total12: u64 = banks.iter().map(|x| max_jolts(12, x)).sum();
 
     println!("Day 3!");
-    println!("Total jolts: {total}");
+    println!("Total jolts 2: {total2}");
+    println!("Total jolts 12: {total12}");
 }
 
-fn max_jolts(bank: &Bank) -> u32 {
-    println!("Calculating max");
-    bank.batteries
-        .iter()
-        .rev()
-        .fold((None, 0), |(prev_max_digit, max_jolts), d| {
-            match prev_max_digit {
-                None => (Some(*d), *d),
-                Some(p) => (Some(max(*d, p)), max(10 * *d + p, max_jolts))
+
+fn max_jolts(size: usize, bank: &Bank) -> u64 {
+
+    let mut result: Vec<u64> = Vec::new(); 
+
+    for &d in bank.batteries.iter() {
+        result.push(d);
+        if result.len() > size {
+            for i in 0..result.len() -1 {
+                if result[i] < result[i+1] {
+                    result.remove(i);
+                    break;
+                }
             }
-        })
-        .1
+            if result.len() > size {
+                result.pop();
+            }
+        }
+    };
+    result.iter().fold(0, |acc, d| 10 * acc + d)
 }
 
 #[cfg(test)]
 mod test03 {
 
     use super::*;
+    fn parsed(s: &str) -> Bank {
+        parse_bank(s).unwrap()
+    }
 
     #[test]
     fn test_max_jolts() {
-        assert_eq!(max_jolts(&parse_bank("987654321111111").unwrap()), 98);
-        assert_eq!(max_jolts(&parse_bank("811111111111119").unwrap()), 89);
-        assert_eq!(max_jolts(&parse_bank("234234234234278").unwrap()), 78);
-        assert_eq!(max_jolts(&parse_bank("818181911112111").unwrap()), 92);
+        assert_eq!(max_jolts(2, &parsed("987654321111111")), 98);
+        assert_eq!(max_jolts(2, &parsed("811111111111119")), 89);
+        assert_eq!(max_jolts(2, &parsed("234234234234278")), 78);
+        assert_eq!(max_jolts(2, &parsed("818181911112111")), 92);
+    }
+
+    #[test]
+    fn test_more_max_jolts() {
+        assert_eq!(max_jolts(12, &parsed("987654321111111")), 987654321111);
+        assert_eq!(max_jolts(12, &parsed("811111111111119")), 811111111119);
+        assert_eq!(max_jolts(12, &parsed("234234234234278")), 434234234278);
+        assert_eq!(max_jolts(12, &parsed("818181911112111")), 888911112111);
     }
 }
