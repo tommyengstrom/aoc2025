262066380e2a6c75c4604917e92521c195f13d9a
Tommy Engström <tommy@succinct.se>
2025-12-15 08:28:49 +0100

    Day 5, part 1

--- src/bin/day05.rs
+++ src/bin/day05.rs
@@ -1,10 +1,18 @@
-//use std::collections::HashSet;
+use std::collections::HashSet;
 use std::fs;
 
 fn main() {
     println!("Day 5");
     let file_content = fs::read_to_string("data/day05.txt").unwrap();
-    let _ = parse_input(&file_content);
+    let (ranges, ids) = match parse_input(&file_content) {
+           Ok(a) => a,
+           Err(err) =>{
+            panic!("Parse error: {err}");
+        },
+        
+    };
+    let fresh_count = fresh_ingredient_count_explicit(&ranges, &ids);
+    println!("There are {fresh_count} fresh ingredients")
 }
 
 fn parse_input(s: &str) -> Result<(Vec<Range>, Vec<u64>), &str> {
@@ -24,10 +32,31 @@ fn parse_input(s: &str) -> Result<(Vec<Range>, Vec<u64>), &str> {
     Ok((ranges, ids))
 }
 
+fn mk_fresh_ingredient_set(ranges: &Vec<Range>) -> HashSet<u64> {
+    ranges.iter().flat_map(|r| r.from..=r.to).collect()
+}
+
 #[derive(Debug, PartialEq)]
 struct Range {
     from: u64,
     to: u64,
+}
+
+fn fresh_ingredient_count_set(ranges: &Vec<Range>, ids: &Vec<u64>) -> usize {
+    let fresh_ingredient_set = mk_fresh_ingredient_set(&ranges);
+    ids.iter().filter(|id| fresh_ingredient_set.contains(id)).count()
+}
+
+fn fresh_ingredient_count_explicit(ranges: &Vec<Range>, ids: &Vec<u64>) -> usize {
+    fn in_some_range(ranges: &Vec<Range>, id: &u64) -> bool {
+        for range in ranges {
+           if range.from <= *id && *id <= range.to {
+            return true
+            }
+        }
+        false
+    }
+    ids.iter().filter(|id| in_some_range(&ranges, id)).count()
 }
 
 #[cfg(test)]
@@ -51,5 +80,32 @@ mod test05 {
         );
 
         assert_eq!(ids, vec![1, 5, 8, 11, 17, 32]);
+    }
+
+    #[test]
+    fn test_fresh_ingredient_count_set() {
+        let ranges = vec![
+                Range { from: 3, to: 5 },
+                Range { from: 10, to: 14 },
+                Range { from: 16, to: 20 },
+                Range { from: 12, to: 18 }];
+        let ids =vec![1, 5, 8, 11, 17, 32];
+        
+        let count = fresh_ingredient_count_set(&ranges, &ids);
+        assert_eq!(count, 3);
+        
+    }
+    #[test]
+    fn test_fresh_ingredient_count_explicit() {
+        let ranges = vec![
+                Range { from: 3, to: 5 },
+                Range { from: 10, to: 14 },
+                Range { from: 16, to: 20 },
+                Range { from: 12, to: 18 }];
+        let ids =vec![1, 5, 8, 11, 17, 32];
+        
+        let count = fresh_ingredient_count_explicit(&ranges, &ids);
+        assert_eq!(count, 3);
+        
     }
 }
