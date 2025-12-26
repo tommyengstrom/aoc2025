1d45a24524d16a0a6e2a7da0480259c6dfc9114c
Tommy Engström <tommy@succinct.se>
2025-12-26 21:53:08 +0100

    day 6 part 2

--- src/bin/day06.rs
+++ src/bin/day06.rs
@@ -3,12 +3,12 @@ use std::fs;
 fn main() {
     println!("Day 6");
 
-    let parsed = parse_input(&fs::read_to_string("data/day06.txt").unwrap());
-    let part1 = sum_of_results(&parsed);
-    println!("Result part 1: {part1}");
-    let transformed: Vec<Equation> = parsed.iter().map(transform_problem).collect();
-    let part2: u64 = sum_of_results(&transformed);
-    println!("Result part 2: {part2}");
+    let parsed = parse_input1(&fs::read_to_string("data/day06.txt").unwrap());
+    let result_1 = part1(&parsed);
+    println!("Result part 1: {result_1}");
+
+    let result_2 = part2(&parse_input_blocks(&fs::read_to_string("data/day06.txt").unwrap()));
+    println!("Result part 2: {result_2}");
 }
 
 #[derive(Debug, PartialEq, Clone)]
@@ -17,54 +17,98 @@ enum Operator {
     Mul,
 }
 
-#[derive(Debug,PartialEq, Clone)]
-struct Equation {
-    numbers: Vec<u64>,
+#[derive(Debug, PartialEq, Clone)]
+struct Equation<T: Clone + PartialEq> {
+    numbers: Vec<T>,
     operator: Operator,
 }
 
-fn parse_input(s: &str) -> Vec<Equation> {
-    let lines: Vec<Vec<&str>> = s.lines().map(|l| l.split_whitespace().collect()).collect();
+
+
+fn parse_input1(s: &str) -> Vec<Equation<u64>> {
+    let blocks = parse_input_blocks(s);
+    blocks
+        .into_iter()
+        .map(|e| Equation {
+            operator: e.operator,
+            numbers: e
+                .numbers
+                .iter()
+                .map(|n| n.trim().parse().unwrap())
+                .collect(),
+        })
+        .collect()
+}
+
+fn parse_input_blocks(s: &str) -> Vec<Equation<&str>> {
+    let lines: Vec<&str> = s.lines().collect();
     let (operators, number_lines) = lines.split_last().unwrap();
+    let lines = number_lines.to_vec();
+    BlockParser { operators, lines }.collect()
+}
 
-    number_lines
-        .iter()
-        .for_each(|l| assert_eq!(l.len(), operators.len(), "Row length mismatch"));
+struct BlockParser<'a> {
+    operators: &'a str,
+    lines: Vec<&'a str>,
+}
 
-    let mut results: Vec<Equation> = vec![];
-    for i in 0..operators.len() {
-        let mut numbers: Vec<u64> = vec![];
-        for l in number_lines {
-            match l[i].parse() {
-                Ok(n) => numbers.push(n),
-                Err(err) => panic!("Unable to parse: {err}"),
-            }
-        }
-        let op = match operators[i] {
-            "*" => Operator::Mul,
-            "+" => Operator::Add,
-            err => panic!("Unexpected operator {err}"),
+impl<'a> Iterator for BlockParser<'a> {
+    type Item = Equation<&'a str>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let op = match self.operators.chars().next()? {
+            '*' => Operator::Mul,
+            '+' => Operator::Add,
+            x => panic!("Invalid operator: {x}"),
         };
-        results.push(Equation {
-            numbers,
+        let next_operator_index = self.operators[1..].find(|c| c != ' ');
+        let length = match next_operator_index {
+            Some(_) => self.operators[1..]
+                .chars()
+                .take_while(|&c| c == ' ')
+                .count(),
+            None => self.operators.len(),
+        };
+        let numbers = self.lines.iter().map(|line| &line[..length]).collect();
+        let skip = if self.operators.len() > length {
+            length + 1
+        } else {
+            length
+        };
+        self.lines = self.lines.iter().map(|line| &line[skip..]).collect();
+        self.operators = &self.operators[skip..];
+        Some(Equation {
             operator: op,
-        });
+            numbers,
+        })
     }
-    results
 }
 
-fn sum_of_results(problems: &[Equation]) -> u64 {
-    problems.iter()
-            .map(|p| match p.operator {
-                Operator::Add => p.numbers.iter().sum(),
-                Operator::Mul => p.numbers.iter().fold(0, |a,b| a * b),
-        }).sum()
+fn part1(problems: &[Equation<u64>]) -> u64 {
+    problems
+        .iter()
+        .map(|p| match p.operator {
+            Operator::Add => p.numbers.iter().sum(),
+            Operator::Mul => p.numbers.iter().fold(0, |a, b| a * b),
+        })
+        .sum()
 }
 
-fn transform_problem(problem: &Equation) -> Equation {
-    problem.clone()
+fn part2(problems: &[Equation<&str>]) -> u64 {
+    problems.iter().map(solve_equation).sum()
 }
 
+fn solve_equation(eq: &Equation<&str>) -> u64 {
+    let mut numbers = vec![];
+    for pos in 0..eq.numbers[0].len() {
+        let apa: String = eq.numbers.iter().map(|&n| n.chars().nth(pos).unwrap()).collect();
+        numbers.push(apa.trim().parse().unwrap());
+    }
+    match eq.operator {
+        Operator::Add => numbers.iter().sum(),
+        Operator::Mul => numbers.iter().product(),
+    }
+}
 
 #[cfg(test)]
 mod test06 {
@@ -73,8 +117,8 @@ mod test06 {
 
     #[test]
     fn test_parse_input() {
-        let input = "123 328  51 64 \n 45 64  387 23 \n 6 98  215 314\n*   +   *   +  ";
-        let parsed = parse_input(&input);
+        let input = "123 328  51 64 \n 45 64  387 23 \n  6 98  215 314\n*   +   *   +  ";
+        let parsed = parse_input1(input);
         assert_eq!(
             parsed[0],
             Equation {
@@ -92,37 +136,50 @@ mod test06 {
         assert_eq!(
             parsed[2],
             Equation {
-                numbers: vec![51,387,215],
+                numbers: vec![51, 387, 215],
                 operator: Operator::Mul
             }
         );
         assert_eq!(
             parsed[3],
             Equation {
-                numbers: vec![64,23,314],
+                numbers: vec![64, 23, 314],
                 operator: Operator::Add
             }
         );
     }
 
     #[test]
-    fn test_transform_problem() {
-        let p1 =Equation { numbers: vec![123, 45, 6], operator: Operator::Mul };
-        let p1_after =Equation { numbers: vec![356, 24, 1], operator: Operator::Mul };
-
-        let p2 = Equation { numbers: vec![328, 64, 98], operator: Operator::Add };
-        let p2_after = Equation { numbers: vec![8, 248, 369], operator: Operator::Add };
-
-        let p3 = Equation { numbers: vec![51,387,215], operator: Operator::Mul };
-        let p3_after = Equation { numbers: vec![175,581,32], operator: Operator::Mul };
-
-        let p4 = Equation { numbers: vec![64,23,314], operator: Operator::Add };
-        let p4_after = Equation { numbers: vec![4,431,623], operator: Operator::Add };
-
-        assert_eq!(transform_problem(&p1), p1_after);
-        assert_eq!(transform_problem(&p2), p2_after);
-        assert_eq!(transform_problem(&p3), p3_after);
-        assert_eq!(transform_problem(&p4), p4_after);
-
+    fn test_parse_input2() {
+        let input = "123 328  51 64 \n 45 64  387 23 \n  6 98  215 314\n*   +   *   +  ";
+        let parsed = parse_input_blocks(input);
+        assert_eq!(
+            parsed[0],
+            Equation {
+                numbers: vec!["123", " 45", "  6"],
+                operator: Operator::Mul
+            }
+        );
+        assert_eq!(
+            parsed[1],
+            Equation {
+                numbers: vec!["328", "64 ", "98 "],
+                operator: Operator::Add
+            }
+        );
+        assert_eq!(
+            parsed[2],
+            Equation {
+                numbers: vec![" 51", "387", "215"],
+                operator: Operator::Mul
+            }
+        );
+        assert_eq!(
+            parsed[3],
+            Equation {
+                numbers: vec!["64 ", "23 ", "314"],
+                operator: Operator::Add
+            }
+        );
     }
 }
